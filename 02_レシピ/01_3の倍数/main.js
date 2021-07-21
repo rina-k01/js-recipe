0020a328723229bd232b8508a2f3cc43cf9af161
rina-k01 <chikichiki200080@gmail.com>
2021-07-21 19:29:27 +0900

    ゼミweek4

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,10 +1,12 @@
 const genkiFunction = function(number) {
   for (let n = 1; n <= number; n++)
-    if (n % 3 === 0) {
+    if (n % 3 === 0 || String(n).indexOf("3") >= 0) {
       console.log(`${n}!!!!!!!`)
     } else {
       console.log(n)
     }
 }
 
-genkiFunction(100)
+setInterval(genkiFunction(40), 1000)
+
+//n <= number;
